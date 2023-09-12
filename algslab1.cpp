7363e0ab3be3315d655ff6c33388843d63551623
MByesMBno <117308282+MByesMBno@users.noreply.github.com>
2023-09-12 12:27:45 +0500

    Add files via upload

--- algslab1.cpp
+++ algslab1.cpp
@@ -65,21 +65,83 @@ void db_FullOrder(double a[], int size, double min, double max){
 		double multiplier= diff / size;
 		a[i]= (multiplier * i) + min;
 	}
-}
+};
 
 void db_ReverseOrder(double a[], int size, double min, double max){
 	db_FullOrder(a, size, max, min);
+};
+
+void ChainSaw(int a[], int size, int min, int max, int step){
+	int h = size/step;
+	for(int i=0; i<step; i++){
+		FullOrder(&a[i*h], h, min, max);
+	}
+	int b=0;
+	if((h*step)<size){
+		double k = ((double(max-min))/h);
+		for(int i= h*step; i<size; i++, b++){
+			a[i]=h * b + min;
+		}
+	}	
+};
+
+void db_ChainSaw(double a[], int size, double min, double max, int step){
+	double h = size/step;
+	int H = size/step;
+	for(int i=0; i<step; i++){
+		db_FullOrder(&a[i*H], h, min, max);
+	}
+	int b=0;
+	if((h*step)<size){
+		double k = ((double(max-min))/h);
+		for(int i= h*step; i<size; i++, b++){
+			a[i]=h * b + min;
+		}
+	}	
+};
+
+
+void Sinus(int a[], int size, int min, int max){
+	
+};
+
+
+void Step(int a[], int size, int min, int max, int step){
+	int h=size/step;
+	for(int i=0; i<size;i++){
+		a[i]=Randomize(min, min + (max/step));
+		if((i+1)%(size/step)==0){
+			min += max / step;
+		}
+	}
+	if((h*step)<size){
+		double m= ((double(max-min))*step/(size));
+		for(int i=h*step; i<size;i++){
+			a[i]=m*(i-h*step)+min;
+		}
+	}
 };
 
 int main(void){
 	int *a= new int[10];
 	double *arr = new double[10];
 	
-	db_FullOrder(arr, 10, 0.1, 1.0);
-	print_db(arr, 10);
+	Step(a, 10, 1, 10, 2);
+	print(a, 10);
 	
-	db_ReverseOrder(arr, 10, 0.1, 1.0);
-	print_db(arr, 10);
+	
+	
+	/*ChainSaw(a, 10, 1, 10, 2); //пилозубчатая
+	print(a, 10);
+	cout<<"//////////////"<<endl;
+	db_ChainSaw(arr, 10, 0.1, 1.0, 2); //пилозубчатая дабл
+	print_db(arr, 10);*/
+	
+	/*db_FullOrder(arr, 10, 0.1, 1.0);
+	print_db(arr, 10);*/
+	
+	/*db_ReverseOrder(arr, 10, 0.1, 1.0);
+	print_db(arr, 10);*/
 	
 	/*db_FullRandom(arr, 10, 0.1, 1.0);
 	print_db(arr, 10);*///рандом double

bdc2ee099f8141951b3954f424905bc7265a31c3
MByesMBno <117308282+MByesMBno@users.noreply.github.com>
2023-09-15 20:47:20 +0500

    Add files via upload

--- algslab1.cpp
+++ algslab1.cpp
@@ -171,16 +171,210 @@ void db_Quas(double a[], int size, double min, double max, double step){
 
 
 int main(void){
-	int const size=30;
-	int *a= new int[size];
-	double *arr = new double[size];
+	int const LENGTH = 6000;
+	int size, min, max, step;
+	double Min, Max;
+	int *a= new int[LENGTH];
+	double *arr = new double[LENGTH];
+	int key;
+	do
+		{
+			cout<<"Input the function:\n"<<" 1: FullRandom():\n 2: FullOrder():\n 3: ReverseOrder():\n 4: ChainSaw():\n 5: Step():\n 6: Sinus():\n 7: Quas():\n 8: db_FullRandom(): \n 9: db_FullOrder(): \n 10: db_ReverseOrder(): \n 11: db_ChainSaw():\n 12: db_Step():\n 13: db_Sinus():\n 14: db_Quas():\n 15: EXIT\n"<<endl;
+			cin>>key;
+			switch(key)
+			{
+				case 1:
+					cout<<"Input the size:"<<endl;
+					cin>>size;
+					cout<<"Input the min of interval:"<<endl;
+					cin>>min;
+					cout<<"Input the max of interval:"<<endl;
+					cin>>max;
+					cout<<"//////////////////////////"<<endl;
+					FullRandom(a, size, min, max);
+					print(a, size);
+				break;
+					
+				case 2:
+					cout<<"Input the size:"<<endl;
+					cin>>size;
+					cout<<"Input the min of interval:"<<endl;
+					cin>>min;
+					cout<<"Input the max of interval:"<<endl;
+					cin>>max;
+					cout<<"//////////////////////////"<<endl;
+					FullOrder(a, size, min, max);
+					print(a, size);
+				break;
+				
+				case 3:
+					cout<<"Input the size:"<<endl;
+					cin>>size;
+					cout<<"Input the min of interval:"<<endl;
+					cin>>min;
+					cout<<"Input the max of interval:"<<endl;
+					cin>>max;
+					cout<<"//////////////////////////"<<endl;
+					ReverseOrder(a, size, min, max);
+					print(a, size);
+				break;
+				
+				case 4:
+					cout<<"Input the size:"<<endl;
+					cin>>size;
+					cout<<"Input the min of interval:"<<endl;
+					cin>>min;
+					cout<<"Input the max of interval:"<<endl;
+					cin>>max;
+					cout<<"Input the step of interval:"<<endl;
+					cin>>step;
+					cout<<"//////////////////////////"<<endl;
+					ChainSaw(a, size, min, max, step);
+					print(a, size);
+				break;
+				
+				case 5:
+					cout<<"Input the size:"<<endl;
+					cin>>size;
+					cout<<"Input the min of interval:"<<endl;
+					cin>>min;
+					cout<<"Input the max of interval:"<<endl;
+					cin>>max;
+					cout<<"Input the step of interval:"<<endl;
+					cin>>step;
+					cout<<"//////////////////////////"<<endl;
+					Step(a, size, min, max, step);
+					print(a, size);
+				break;
+				
+				case 6:
+					cout<<"Input the size:"<<endl;
+					cin>>size;
+					cout<<"Input the min of interval:"<<endl;
+					cin>>min;
+					cout<<"Input the max of interval:"<<endl;
+					cin>>max;
+					cout<<"//////////////////////////"<<endl;
+					Sinus(a, size, min, max);
+					print(a, size);
+				break;
+				
+				case 7:
+					cout<<"Input the size:"<<endl;
+					cin>>size;
+					cout<<"Input the min of interval:"<<endl;
+					cin>>min;
+					cout<<"Input the max of interval:"<<endl;
+					cin>>max;
+					cout<<"Input the step of interval:"<<endl;
+					cin>>step;
+					cout<<"//////////////////////////"<<endl;
+					Quas(a, size, min, max, step);
+					print(a, size);
+				break;
+				
+				case 8:
+					cout<<"Input the size:"<<endl;
+					cin>>size;
+					cout<<"Input the min of interval:"<<endl;
+					cin>>Min;
+					cout<<"Input the max of interval:"<<endl;
+					cin>>Max;
+					cout<<"//////////////////////////"<<endl;
+					db_FullRandom(arr, size, Min, Max);
+					print_db(arr, size);
+				break;
+				
+				case 9:
+					cout<<"Input the size:"<<endl;
+					cin>>size;
+					cout<<"Input the min of interval:"<<endl;
+					cin>>Min;
+					cout<<"Input the max of interval:"<<endl;
+					cin>>Max;
+					cout<<"//////////////////////////"<<endl;
+					db_FullOrder(arr, size, Min, Max);
+					print_db(arr, size);
+				break;
+				
+				case 10:
+					cout<<"Input the size:"<<endl;
+					cin>>size;
+					cout<<"Input the min of interval:"<<endl;
+					cin>>Min;
+					cout<<"Input the max of interval:"<<endl;
+					cin>>Max;
+					cout<<"//////////////////////////"<<endl;
+					db_ReverseOrder(arr, size, Min, Max);
+					print_db(arr, size);
+				break;
+				
+				case 11:
+					cout<<"Input the size:"<<endl;
+					cin>>size;
+					cout<<"Input the min of interval:"<<endl;
+					cin>>Min;
+					cout<<"Input the max of interval:"<<endl;
+					cin>>Max;
+					cout<<"//////////////////////////"<<endl;
+					db_ChainSaw(arr, size, Min, Max, step);
+					print_db(arr, size);
+				break;
+				
+				case 12:
+					cout<<"Input the size:"<<endl;
+					cin>>size;
+					cout<<"Input the Min of interval:"<<endl;
+					cin>>Min;
+					cout<<"Input the Max of interval:"<<endl;
+					cin>>Max;
+					cout<<"Input the step of interval:"<<endl;
+					cin>>step;
+					cout<<"//////////////////////////"<<endl;
+					db_Step(arr, size, Min, Max, step);
+					print_db(arr, size);
+				break;
+				
+				case 13:
+					cout<<"Input the size:"<<endl;
+					cin>>size;
+					cout<<"Input the min of interval:"<<endl;
+					cin>>Min;
+					cout<<"Input the max of interval:"<<endl;
+					cin>>Max;
+					cout<<"//////////////////////////"<<endl;
+					db_Sinus(arr, size, Min, Max);
+					print_db(arr, size);
+				break;
+				
+				case 14:
+					cout<<"Input the size:"<<endl;
+					cin>>size;
+					cout<<"Input the min of interval:"<<endl;
+					cin>>Min;
+					cout<<"Input the max of interval:"<<endl;
+					cin>>Max;
+					cout<<"Input the step of interval:"<<endl;
+					cin>>step;
+					cout<<"//////////////////////////"<<endl;
+					db_Quas(arr, size, Min, Max, step);
+					print_db(arr, size);
+				break;
+				
+				case 15:
+					cout<<"Welcome back"<<endl;
+				break;
+		
+				}
+		}
+while(key!=15);
 	
 	
-	Quas(a, size, 1, 32, 2);
+	/*Quas(a, size, 1, 32, 2);
 	print(a, size);
 	cout<<"///////"<<endl;
 	db_Quas(arr, size, 0.1, 30.5, 0.5);
-	print_db(arr, size);
+	print_db(arr, size);*/
 	/*FullRandom(a,size, 1, 10);
 	print(a, size);*/
 	
